1f85cdcbc2841bce9e2a6b91e5d5725a527917da
Стас Пивоваров <stas.povovarov@headmade.pro>
2024-07-17 13:21:32 +0300

    feat: change config

--- next.config.cjs
+++ next.config.cjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-import path from 'path';
-import {fileURLToPath} from 'url';
-const __filename = fileURLToPath(import.meta.url);
+const path = require('path')
 
-const __dirname = path.dirname(__filename);
 
 const nextConfig = {
   output: 'export',
